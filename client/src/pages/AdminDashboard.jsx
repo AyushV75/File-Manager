eb2bf2365728324cc05227fcc64e7951a63887e4
Ayush Vekariya <ayushvekariya629@gmail.com>
2026-09-24 12:19:05 +0530

    Fix: Admin url changes

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -11,7 +11,7 @@ import {
   Shield,
 } from "lucide-react";
 
-const API_URL = "http://localhost:5000/api/admin";
+const API_URL = `${import.meta.env.VITE_API_URL}/admin`;
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
